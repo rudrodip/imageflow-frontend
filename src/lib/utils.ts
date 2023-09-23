a111f51ac73d496c3bbcaf0ab9732cfe8604d7fc
rudrodip <official.rudrodipsarker@gmail.com>
2023-09-23 15:03:02 +0600

    dirty fix :)

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,6 +6,6 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export function absoluteUrl(path: string) {
-  return `${env.NEXT_PUBLIC_APP_URL}${path}`
-}
+// export function absoluteUrl(path: string) {
+//   return `${env.NEXT_PUBLIC_APP_URL}${path}`
+// }
